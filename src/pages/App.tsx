5823960a61da2001706c669cca658166aee816c7
thanveer.ahamed <thanveer.ahamed@nn.nl>
2023-12-06 20:23:32 +0530

    feature: station selection dialog

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header.tsx';
 import FullPageLoader from '../components/loaders/FullPageLoader.tsx';
@@ -8,16 +9,31 @@ import { auth } from '../services/firebase.ts';
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
+  const [signOut] = useSignOut(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (error) {
+      (async () => {
+        await signOut();
+        navigate('/login');
+      })();
+    }
+  }, [error, navigate, signOut]);
 
   if (loading) {
     return <FullPageLoader />;
   }
 
+  if (!user) {
+    return <Navigate to={'/login'} />;
+  }
+
   return (
     <>
-      <Header />
+      {location.pathname !== '/' && <Header />}
       {user && <Outlet />}
-      {error && <p>Error</p>}
     </>
   );
 }
