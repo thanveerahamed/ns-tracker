2a19a5cfe62c383637abddccc633695a6ddba28f
thanveer.ahamed <thanveer.ahamed@nn.nl>
2023-12-12 21:19:07 +0530

    fix: pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/index.html',
+    port: 5174,
+  },
+  build: {
+    outDir: 'build',
+  },
 });
