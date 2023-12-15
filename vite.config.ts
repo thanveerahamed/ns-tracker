4cf94e2ac6fa7c924d3acc0edf8ddc2afb3995ce
thanveer.ahamed <thanveer.ahamed@nn.nl>
2023-12-15 19:47:34 +0530

    fix: add vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import react from '@vitejs/plugin-react';
 
 import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['masked-icon.svg'],
+  manifest: {
+    name: 'NS Tracker App',
+    short_name: 'NS Tracker',
+    description: 'An app that can help with better NS Train tracking',
+    icons: [
+      {
+        src: '/train_256.png',
+        sizes: '256X256',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   server: {
     open: '/index.html',
     port: 5174,
