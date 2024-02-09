edee4621412d46f0bd4cd30d1683041038b42372
thanveer.ahamed <thanveer.ahamed@nn.nl>
2024-02-09 10:06:40 +0100

    fix: route

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
   }, [recentSearchSnapshots]);
 
   const handleSearch = () => {
-    navigate('/tripsInformation');
+    navigate('/trips');
   };
 
   const handleRecentSearchClicked = (props: UpdateRecentSearchProps) => {
